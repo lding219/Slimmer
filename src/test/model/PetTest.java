93ca332fc24c35cac7c075f01c769c95361dd395
Liyuan Ding <dly_219@outlook.com>
2024-10-06 15:10:28 -0700

    Updated PetTest based on the new eatfood method

--- src/test/model/PetTest.java
+++ src/test/model/PetTest.java
@@ -11,12 +11,14 @@ public class PetTest {
     private Pet testPet;
     private Food f1;
     private Food f2;
+    private Food f3;
 
     @BeforeEach
     void runBefore() {
         testPet = new Pet("Lucky");
         f1 = new Food("Strawberry", 1, "Tuesday");
         f2 = new Food("fish", 0, "Wednesday");
+        f3 = new Food("Strawberry", 2, "Wednesday");
     }
 
     @Test
@@ -44,6 +46,7 @@ public class PetTest {
         assertEquals(7, foods.get(0).getFoodAmount());
     }
 
+
     // Test the case when the pet have multiple food for multiple times
     @Test
     void testEatMultipleFood() {
@@ -55,6 +58,7 @@ public class PetTest {
         assertEquals(4, foods.get(0).getFoodAmount());
         assertEquals(3, foods.get(1).getFoodAmount());
     }
+    // Test the case when the pet eats the pre-existed food on another day
 
     @Test
     void testViewDailyFoods() {
@@ -71,5 +75,16 @@ public class PetTest {
         testPet.viewDailyFoods("Wednesday");
         ArrayList<Food> wednesdayFoods = testPet.viewDailyFoods("Wednesday");
         assertTrue(wednesdayFoods.isEmpty());
+    }
+    @Test
+    void testEatAnotherDay() {
+        testPet.eatFood(f1, 3);
+        testPet.eatFood(f3, 3);
+        ArrayList<Food> tuesdayFoods = testPet.viewDailyFoods("Tuesday");
+        ArrayList<Food> wednesdayFoods = testPet.viewDailyFoods("Wednesday");
+        assertEquals(f1, tuesdayFoods.get(0));
+        assertEquals(f3, wednesdayFoods.get(0));
+        assertEquals(4, tuesdayFoods.get(0).getFoodAmount());
+        assertEquals(5, wednesdayFoods.get(0).getFoodAmount());
     }
 }
