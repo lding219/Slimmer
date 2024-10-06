d18aaeeec6ad93bc579d2a180aa6bffbb12c8081
Liyuan Ding <dly_219@outlook.com>
2024-10-05 23:08:33 -0700

    added viewDailyFoods method

--- src/main/model/Pet.java
+++ src/main/model/Pet.java
@@ -1,33 +1,47 @@
 package model;
+
 import java.util.*;
+
 public class Pet {
     private String petName; // name of the pet
     private ArrayList<Food> foods; // the list of food the cat has had today
 
+    /*
+     * REQUIRES: petName has a non-zero length
+     * EFFECTS: name on the pet is set to petName ; set foods be
+     * an empty list
+     */
+    public Pet(String petName) {
+        this.petName = petName;
+        this.foods = new ArrayList<>();
+    }
 
-/*
- * REQUIRES: petName has a non-zero length
- * EFFECTS: name on the pet is set to petName ; set foods be
- * an empty list
- */
-public Pet(String petName){
-    this.petName = petName;
-    this.foods = new ArrayList<>();
-}
-// REQUIRES: amount>=0
-// MODIFIES: this, food
-// EFFECTS: the food is added to the list of the food the pet has had,
-// and docoment the amount.
-public void eatFood(Food food, int amount){ 
-    this.foods.add(food);
-    food.increaseAmountBy(amount);
-}
+    // REQUIRES: amount>=0
+    // MODIFIES: this, food
+    // EFFECTS: the food is added to the list of the food the pet has had,
+    // and docoment the amount.
+    public void eatFood(Food food, int amount) {
+        this.foods.add(food);
+        food.increaseAmountBy(amount);
+    }
 
-public String getPetName(){
-    return petName;
+    // REQUIRES: day must be a day in the week, eg. Monday
+    // EFFECT: show all the food the pet has had in the given day
+    public ArrayList<Food> viewDailyFoods(String day) {
+        ArrayList<Food> dailyFoods = new ArrayList<>();
+       for (Food food : foods) {
+        if(food.getDayName().equals(day)){
+            dailyFoods.add(food);
+        }
+       }
+       return dailyFoods;
+    }
 
-}
-public ArrayList<Food> getFoods(){
-    return foods;
+    public String getPetName() {
+        return petName;
+
+    }
+    public ArrayList<Food> getFoods() {
+        return foods;
 }
 }
