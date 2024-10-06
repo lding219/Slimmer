074d50d01166d6033541f6bcc9c24466386608b6
Liyuan Ding <dly_219@outlook.com>
2024-10-06 12:43:50 -0700

    solved checkstyle problem for food class

--- src/main/model/Food.java
+++ src/main/model/Food.java
@@ -1,33 +1,35 @@
 package model;
+
 public class Food {
-    private int foodAmount; //the food amount the pet has had
-    private String foodName;//the name of the food
+    private int foodAmount; // the food amount the pet has had
+    private String foodName;// the name of the food
     private String dayName;// the day the food being eaten
-    
 
     // REQUIRES: foodAmount >=0, foodName has a non-zero length
     // EFFECTS: constructs food with foodName, foodAmount, and
     // the day the food being eaten
-public Food(String foodName, int foodAmount, String dayName){
-    this.foodName = foodName;
-    this.foodAmount = foodAmount;
-    this.dayName = dayName;
-}
+    public Food(String foodName, int foodAmount, String dayName) {
+        this.foodName = foodName;
+        this.foodAmount = foodAmount;
+        this.dayName = dayName;
+    }
+
     // REQUIRES: amount >=0
     // MODIFIES: this
     // EFFECTS: increase the foodAmount for this food by amount
-public void increaseAmountBy(int amount){
-    this.foodAmount += amount;
-}
-public String getFoodName(){
-    return foodName;
-}
-public int getFoodAmount(){
-    return foodAmount;
-}
-public String getDayName() {
-    return dayName;
-}
-}
+    public void increaseAmountBy(int amount) {
+        this.foodAmount += amount;
+    }
 
+    public String getFoodName() {
+        return foodName;
+    }
 
+    public int getFoodAmount() {
+        return foodAmount;
+    }
+
+    public String getDayName() {
+        return dayName;
+    }
+}
