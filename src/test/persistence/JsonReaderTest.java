ad3e47f2c116a75aa620f782e4e49791ad8ed997
Liyuan Ding <dly_219@outlook.com>
2024-10-19 19:03:47 -0700

    updated JsonReaderTest

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -39,20 +39,14 @@ public class JsonReaderTest extends JsonTest {
         JsonReader reader = new JsonReader("./data/testReaderGeneralHome.json");
         try {
             Home h = reader.read();
-            assertEquals(2, h.getHomeSize());
+            assertEquals(1, h.getHomeSize());
             ArrayList<Pet> pets = h.viewHome();
             ArrayList<Food> foodsLucky = new ArrayList<>();
-            ArrayList<Food> foodsTutu = new ArrayList<>();
             Food apple = new Food("apple", 10, "Monday");
             Food bone = new Food("bone", 1, "Tuesday");
-            Food treats = new Food("treats", 5, "Wednesday");
-            Food fish = new Food("fish", 2, "Friday");
             foodsLucky.add(apple);
             foodsLucky.add(bone);
-            foodsTutu.add(treats);
-            foodsTutu.add(fish);
             checkPet("Lucky", 1, foodsLucky, pets.get(0));
-            checkPet("Tutu", 2, foodsTutu, pets.get(1));
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
