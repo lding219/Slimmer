60dd46377a315c7edef6b82da7a9928f1db3c8bc
Liyuan Ding <dly_219@outlook.com>
2024-10-19 19:10:55 -0700

    updated Main method

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,13 @@
 package ui;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
-        new SlimmerApp();
+        try {
+            new SlimmerApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
     }
 }
