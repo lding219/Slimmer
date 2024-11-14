9c222400a71e3b2ce645c26ffe6f089e8f2f2dff
Liyuan Ding <dly_219@outlook.com>
2024-11-13 21:42:06 -0800

    added test for the getter method and the removePet method

--- src/test/model/HomeTest.java
+++ src/test/model/HomeTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,6 +35,32 @@ public class HomeTest {
         assertEquals(p1, pets.get(0));
         assertEquals(p2, pets.get(1));
 
+    }
+
+    @Test
+    void testRemovePet() {
+        testHome.addPet(p1);
+        testHome.addPet(p2);
+        assertEquals(2, testHome.getHomeSize());
+        ArrayList<Pet> pets = testHome.viewHome();
+        assertEquals(p1, pets.get(0));
+        assertEquals(p2, pets.get(1));
+        testHome.removePet(p1);
+        ArrayList<Pet> petsAfterRemove = testHome.viewHome();
+        assertEquals(1, testHome.getHomeSize());
+        assertEquals(p2, petsAfterRemove.get(0));
+    }
+
+    @Test
+    void testGetPet() {
+        testHome.addPet(p1);
+        testHome.addPet(p2);
+        ArrayList<Pet> pets = testHome.viewHome();
+        Pet petOne = testHome.getPet("Lucky");
+        assertEquals(petOne, pets.get(0));
+        Pet petTwo = testHome.getPet("Tutu");
+        assertEquals(petTwo, pets.get(1));
+        assertNull(testHome.getPet("Pika"));
     }
 
 }
