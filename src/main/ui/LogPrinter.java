efc91b1e21086e09401c785fe8ee33256407831d
Liyuan Ding <dly_219@outlook.com>
2024-11-25 22:54:59 -0800

    added reference information

--- src/main/ui/LogPrinter.java
+++ src/main/ui/LogPrinter.java
@@ -3,14 +3,17 @@ package ui;
 import model.EventLog;
 import model.exception.LogException;
 
+// Referenced from the AlarmSystem
+// https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
 /**
  * Defines behaviours that event log printers must support.
  */
 public interface LogPrinter {
-	/**
-	 * Prints the log
-	 * @param el  the event log to be printed
-	 * @throws LogException when printing fails for any reason
-	 */
+    /**
+     * Prints the log
+     * 
+     * @param el the event log to be printed
+     * @throws LogException when printing fails for any reason
+     */
     void printLog(EventLog el) throws LogException;
 }
